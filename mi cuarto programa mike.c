f33ea9ecc5fbc8e8b7c62b009746de1414c98144
MiguelDeLunaPonce2007 <64348087+MiguelDeLunaPonce2007@users.noreply.github.com>
2020-04-27 20:22:29 -0500

    Add files via upload

---
+++ mi cuarto programa mike.c
@@ -0,0 +1,11 @@
+//the dinosaurs
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+    printf("The dinosaurs\n");
+    printf("The\n dinosaurs\n are\n so\n dangerous\n but\n they\n are\n dead\n so\n they\n can\n not\n hurt\n us\n");
+
+    return 0;
+}
